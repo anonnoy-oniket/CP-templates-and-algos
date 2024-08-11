e9473835aa6f298b7b11f82c3c51ce0469541ac1
Anonnoy Oniket <70757271+anonnoy-oniket@users.noreply.github.com>
2024-08-12 01:13:35 +0600

    Create Boilerplate.cpp

---
+++ Templates/Boilerplate.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+#ifndef ONLINE_JUDGE
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+
+#define __fast_io ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count()); //mersenne twister engine
+
+void __init_code()
+{
+    __fast_io;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void SOLVE()
+{   
+    
+}
+
+int main()
+{
+    __init_code();
+    auto start = high_resolution_clock::now();
+    int t = 1;
+    cin >> t;
+    while (t--) SOLVE();
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop-start);
+    #ifndef ONLINE_JUDGE
+        cout << endl << endl << "Time: " << 1.0*duration.count() / 1000 << " ms" << endl;
+    #endif    
+
+    return 0;
+}
