cd55c33bc63e9d72378af5587fb3a0c91b12bb9c
Anonnoy Oniket <70757271+anonnoy-oniket@users.noreply.github.com>
2024-11-08 12:29:25 +0600

    Create segment tree lazy.cpp

---
+++ tree/segment tree lazy.cpp
@@ -0,0 +1,54 @@
+const int maxN = 1e5 + 9;
+long long a[maxN], t[maxN * 4], lazy[maxN * 4];
+
+void push(int n, int b, int e) {
+   if (lazy[n] == 0) {
+      return;
+   }
+   t[n] += (1LL * (e - b + 1) * lazy[n]);
+   if (b != e) {
+      int l = (2 * n), r = (2 * n) + 1;
+      lazy[l] += lazy[n];
+      lazy[r] += lazy[n];
+   }
+   lazy[n] = 0;
+}
+
+void build(int n, int b, int e) {
+   if (b == e) {
+      t[n] = a[b];
+      return;
+   }
+   int mid = (b + e) / 2, l = (2 * n), r = (2 * n) + 1;
+   build(l, b, mid);
+   build(r, mid + 1, e);
+   t[n] = t[l] + t[r];
+}
+
+void update(int n, int b, int e, int i, int j, int v) {
+   push(n, b, e);
+   if (e < i || j < b) {
+      return;
+   }
+   if (b >= i && e <= j) {
+      lazy[n] = v;
+      push(n, b, e);
+      return;
+   }
+   int mid = (b + e) / 2, l = (2 * n), r = (2 * n) + 1;
+   update(l, b, mid, i, j, v);
+   update(r, mid + 1, e, i, j, v);
+   t[n] = t[l] + t[r];
+}
+
+long long query(int n, int b, int e, int i, int j) {
+   push(n, b, e);
+   if (e < i || j < b) {
+      return 0;
+   }
+   if (b >= i && e <= j) {
+      return t[n];
+   }
+   int mid = (b + e) / 2, l = (2 * n), r = (2 * n) + 1;
+   return query(l, b, mid, i, j) + query(r, mid + 1, e, i, j);
+}
